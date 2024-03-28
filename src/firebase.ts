cda08efb34aa3ca52506f0152774515a599930d5
R-unic <roleyspersonal@gmail.com>
2024-03-27 20:48:44 -0400

    ref: move Firebase class to it's own file

---
+++ src/firebase.ts
@@ -0,0 +1,43 @@
+export class Firebase {
+  private readonly auth = `.json?auth=${process.env.FIREBASE_AUTH}`;
+
+  public constructor(
+    private readonly name: string,
+    private readonly baseURL: string
+  ) {
+    this.baseURL = baseURL.endsWith("/") ? baseURL : baseURL + "/";
+  }
+
+  public async set<T>(path?: string, value?: T, headers?: Record<string, string>): Promise<void> {
+    if ((value instanceof Array && value.length === 0) || (value instanceof Object && Object.entries(value).length === 0))
+      return this.delete(path);
+
+    await fetch(this.getEndpoint(path), {
+      method: "PUT",
+      body: JSON.stringify(value),
+      headers
+    }).catch(err => console.warn(`[Firebase] Error: ${err}`));
+  }
+
+  public async get<T>(path?: string, defaultValue?: T): Promise<T> {
+    return <T>await fetch(this.getEndpoint(path), { method: "GET" })
+      .then(res => res.json()) ?? defaultValue!;
+  }
+
+  public async delete(path?: string): Promise<void> {
+    await this.set(path, undefined, { "X-HTTP-Method-Override": "DELETE" });
+  }
+
+  public async increment(path?: string, delta = 1): Promise<void> {
+    await this.set(path, await this.get<number>(path) + delta);
+  }
+
+  public async addToArray<T>(path: string, value: T): Promise<void> {
+    const data = await this.get<T[]>(path, []);
+    this.set(`${path}/${data.length}`, value);
+  }
+
+  private getEndpoint(path?: string): string {
+    return this.baseURL + this.name + encodeURIComponent(path === undefined ? "" : `/${path}`) + this.auth;
+  }
+}
