5741d5a6d338bf7dc84be595d3ddf2f1e382cb1f
R-unic <roleyspersonal@gmail.com>
2024-03-29 20:36:11 -0400

    fix: import in reload-commands.ts

--- src/reload-commands.ts
+++ src/reload-commands.ts
@@ -1,10 +1,10 @@
 import { REST, Routes } from "discord.js";
 import { configDotenv } from "dotenv";
-import { client } from "./main";
+import { client } from "./main.js";
 
 configDotenv();
 const rest = (new REST).setToken(process.env.TOKEN!);
 rest.put(
   Routes.applicationCommands(client.user?.id!),
-  { body: client.applicationCommands },
+  { body: client.applicationCommands }
 );
