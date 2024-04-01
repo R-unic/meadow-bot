40ca0191feab1be87824b7f7ed22032706c2d9d0
R-unic <roleyspersonal@gmail.com>
2024-04-01 00:35:40 -0400

    feat: add error handling to data commands

--- src/commands/meta/data.ts
+++ src/commands/meta/data.ts
@@ -1,5 +1,5 @@
 import { Discord, Guard, Slash, SlashGroup, SlashOption } from "discordx";
-import { Category, PermissionGuard } from "@discordx/utilities";
+import { Category } from "@discordx/utilities";
 import { ApplicationCommandOptionType, type CommandInteraction } from "discord.js";
 
 import { Firebase } from "../../firebase.js";
@@ -28,16 +28,22 @@ export class Data {
   ): Promise<void> {
     if (!command.channel) return;
 
-    const db = new Firebase(process.env.FIREBASE_URL!);
-    const result = await db.get(path);
-    if (result === undefined)
-      return void await command.reply({
-        embeds: [Embed.error(`There is no data at path \`${path}\`.`)]
-      });
+    try {
+      const db = new Firebase(process.env.FIREBASE_URL!);
+      const result = await db.get(path);
+      if (result === undefined)
+        return void await command.reply({
+          embeds: [Embed.error(`There is no data at path \`${path}\`.`)]
+        });
 
-    await command.reply({
-      embeds: [Embed.success(`\`\`\`json\n${JSON.stringify(result, undefined, 2).slice(0, 4095)}\`\`\``)]
-    });
+      await command.reply({
+        embeds: [Embed.success(`\`\`\`json\n${JSON.stringify(result, undefined, 2).slice(0, 4095)}\`\`\``)]
+      });
+    } catch (error) {
+      await command.reply({
+        embeds: [Embed.error(<string>error)]
+      });
+    }
   }
 
   @Slash({ description: "Delete the data at `path`" })
@@ -54,16 +60,22 @@ export class Data {
   ): Promise<void> {
     if (!command.channel) return;
 
-    const db = new Firebase(process.env.FIREBASE_URL!);
-    const result = await db.get(path);
-    if (result === undefined)
-      return void await command.reply({
-        embeds: [Embed.error(`There is no data at path \`${path}\`.`)]
-      });
+    try {
+      const db = new Firebase(process.env.FIREBASE_URL!);
+      const result = await db.get(path);
+      if (result === undefined)
+        return void await command.reply({
+          embeds: [Embed.error(`There is no data at path \`${path}\`.`)]
+        });
 
-    await db.delete(path);
-    await command.reply({
-      embeds: [Embed.success(`Successfully deleted data at path \`${path}\`!`)]
-    });
+      await db.delete(path);
+      await command.reply({
+        embeds: [Embed.success(`Successfully deleted data at path \`${path}\`!`)]
+      });
+    } catch (error) {
+      await command.reply({
+        embeds: [Embed.error(<string>error)]
+      });
+    }
   }
 }
